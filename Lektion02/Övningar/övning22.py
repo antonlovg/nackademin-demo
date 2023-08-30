9bf9688d364196b2a7ace472deddaa0f1aa0c990
Anton Lövgren <karlanton.lovgren@gmail.com>
2023-08-30 13:32:07 +0200

    Delar upp övningar

---
+++ Lektion02/Övningar/övning22.py
@@ -0,0 +1,2 @@
+flyttal = float(input("Vänligen skriv in ett flyttal: ")) # En float-input där användare kan ange tal med decimaler
+print(int(flyttal)) # Gör om float till int (int = heltal endast)
