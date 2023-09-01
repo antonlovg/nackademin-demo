756264a5f6541ce9936a67ca3c0f76bbdc540986
Anton Lövgren <karlanton.lovgren@gmail.com>
2023-09-01 12:56:58 +0200

    Rättning av felkod

--- Lektion03/Övningar/övning34.py
+++ Lektion03/Övningar/övning34.py
@@ -1,9 +1,9 @@
 land = input("Vänligen ange ett land för att se om det tillhör Norden eller Storbritannien: ").lower()
 
-if land == ("danmark" or "finland" or "island" or "norge" or "sverige"):
+if land == "danmark" or "finland" or "island" or "norge" or "sverige":
     print("Detta land tillhör Norden.")
     exit()
-elif land == ("england" or "nordirland" or "skottland" or "wales"):
+elif land == "england" or "nordirland" or "skottland" or "wales":
     print("Detta land tillhör Storbritannien.")
     exit()
 print("Detta land tillhör varken Storbritannien eller Norden.")
