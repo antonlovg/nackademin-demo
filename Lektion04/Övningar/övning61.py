0d13df82842318510f154eb2438f14ae5e3e6a46
Anton Lövgren <karlanton.lovgren@gmail.com>
2023-09-04 18:46:39 +0200

    while loop inlagd

---
+++ Lektion04/Övningar/övning61.py
@@ -0,0 +1,18 @@
+# Övning 6.1
+"""
+ange_text = [input("Ange en text: ")]
+text_längd = len(ange_text)
+ange_bokstav = input("Ange en bokstav: ")
+
+print(len(ange_text))
+
+"""
+
+angeText = [input("Ange en text: ")]
+angeBokstav = input("Ange en bokstav: ")
+
+n = 0
+while n < len(angeText):
+
+    n =+ 1
+
